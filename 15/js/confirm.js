64478acb303f1d7bcad1edda39289a833c6b752b
HRobbie <hwwyouxiang@163.com>
2017-01-13 17:34:11 +0800

    这是第一次提交

---
+++ 15/js/confirm.js
@@ -0,0 +1,9 @@
+$(function(){
+	$("#stop_text").html("未知的移动终端");
+	$("#stop_dialog").fadeIn(300);
+//	$("#stop_dialog").fadeIn(300);
+	
+	$("#stop_fail").click(function(){
+		$("#stop_dialog").fadeOut(300);
+	});
+})
---
+++ 15/js/error.js
@@ -0,0 +1,17 @@
+var first=true;
+function loadingAgain(){
+	try{
+		if(first){		
+			javascript:Android.loadingAgain();
+			first=false;
+			$('#stop').addClass('disable');
+		}
+	}catch(e){
+		//TODO handle the exception
+		console.log(e.message);
+	}
+	
+	console.log("调用了重新加载的方法");
+
+}
+
